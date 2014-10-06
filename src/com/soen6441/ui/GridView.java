3459ce486591fc45ccd53cac3b71b7842dbf2e0f
Zhe Zhao <zhaozheid@gmail.com>
2014-10-06 00:45:43 -0400

    First Commit

---
+++ src/com/soen6441/ui/GridView.java
@@ -0,0 +1,7 @@
+package com.soen6441.ui;
+
+import com.soen6441.ui.parallel.View;
+
+public class GridView extends View {
+
+}
---
+++ src/com/soen6441/ui/parallel/View.java
@@ -0,0 +1,7 @@
+package com.soen6441.ui.parallel;
+
+import javax.swing.JPanel;
+
+public class View extends JPanel {
+
+}
