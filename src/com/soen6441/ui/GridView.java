b176a3dd8e1c98bd594873a39b9e372e58b1fdcb
jeanraymonddaher <jeanraymond.daher@live.com>
2014-10-07 17:09:08 -0400

    fix stuff

--- src/com/soen6441/ui/GridView.java
+++ src/com/soen6441/ui/GridView.java
@@ -2,12 +2,6 @@ package com.soen6441.ui;
 import com.soen6441.ui.parallel.View;
 
 /**
-<<<<<<< Updated upstream
- * @author chenglong zhang chenglongzhang931@gmail.com
- * @since version 1.0
- * @see 
- */
-=======
  * This class defines the GridView, the main grid, map of the game.
  * 
  * @author JeanRaymondDaher
@@ -15,7 +9,7 @@ import com.soen6441.ui.parallel.View;
  * @see GridView
  */
 
->>>>>>> Stashed changes
+
 public class GridView extends View {
 
 	private GridViewCell selectedCell;
