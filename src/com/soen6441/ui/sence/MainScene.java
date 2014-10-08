699880d64774cd244a9b6c6c9cc7936e0ed13eee
alexwolfwang <alexwolfwang@gmail.com>
2014-10-08 15:11:59 -0400

    add new classes
    add two classes MainScene and NewMapScene class to Scene package

---
+++ src/com/soen6441/ui/sence/MainScene.java
@@ -0,0 +1,23 @@
+package com.soen6441.ui.sence;
+
+import com.soen6441.ui.parallel.*;
+
+/**
+ * @author Mengyao Wang
+ * @version jdk 1.7
+ * @see 
+ * This class defines the mainscene. On the Main interface of our game, 
+ * there are 3 buttons presented: Playbutton, Editbutton and Newmapbutton
+ *  
+*/
+
+public class MainScene extends View{
+	
+	private Button playButton;
+	private Button editButton;
+	private Button newMapButton;
+
+}
+
+
+
