69cde5e58f8c90c546ce739ffd6a08566e50c9ee
shyqqtt <shyqqtt@gmail.com>
2014-10-08 18:34:09 -0400

    add comments

--- Towers/TowerManagerFactory.java
+++ Towers/TowerManagerFactory.java
@@ -2,15 +2,41 @@ package com.soen6441.Towers;
 
 import java.util.Map;
 
+/**
+ * This class is used to define TowerManager.
+ * 
+ * @author Haiyang Sun
+ *
+ */
+
 public class TowerManagerFactory {
+	
+	/**
+	 * The array is used to store type names.
+	 */
+	public String[] typeNames;
+	
+	/**
+	 * The map is used to store TowerManager objects and its names.
+	 */
+	private Map<String, TowerManager> managers;
+	
+	
+	/**
+	 * 
+	 * @return TowerManagerFactory
+	 */
 
 	public static TowerManagerFactory defaultFactory() {
 		return null;
 	}
 	
-	public String[] typeNames;
-	
-	private Map<String, TowerManager> managers;
+	/**
+	 * This method is used to get a certain type of TowerManager by its type name.
+	 * 
+	 * @param typeName
+	 * @return Return a TowerManager object
+	 */
 	
 	public TowerManager getManager(String typeName) {
 		return null;
