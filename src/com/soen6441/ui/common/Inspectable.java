ba31474b48f4bf4259cd78c39bf16906f9e74489
jeanraymonddaher <jeanraymond.daher@live.com>
2014-10-08 13:05:13 -0400

    re

--- src/com/soen6441/ui/common/Inspectable.java
+++ src/com/soen6441/ui/common/Inspectable.java
@@ -1,5 +1,20 @@
 package com.soen6441.ui.common;
 
-public interface IInspectable {
+//not finish
+/**
+ * This is the interface inspectable. Every object that implements inspectable will have options displayed on the right side, as explained in Inspector view.
+ * @author chenglong zhang chenglongzhang931@gmail.com , jean raymond daher
+ * @since version 1.0
+ * @see InspectorView
+ */
+public interface Inspectable {
+
+	public String title;
+	public String subtitle;
+	public Image image;
+	public String description;
+	public Command[] commands;
+	
+	public void execute (Command command);
 
 }
