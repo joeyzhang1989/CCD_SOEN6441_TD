c7781469257ac44c61e86548a5bc057ea50071e7
chenglong zhang <chenglongzhang931@gmail.com>
2014-10-08 20:04:43 -0400

    comment change

--- src/com/soen6441/ui/common/IInspectable.java
+++ src/com/soen6441/ui/common/IInspectable.java
@@ -5,7 +5,7 @@ import java.util.List;
 //not finish
 /**
  * This is the interface inspectable. Every object that implements inspectable will have options displayed on the right side, as explained in Inspector view.
- * @author chenglong zhang chenglongzhang931@gmail.com , jean raymond daher
+ * @author chenglong zhang, jean raymond daher
  * @since version 1.0
  * @see InspectorView
  */
