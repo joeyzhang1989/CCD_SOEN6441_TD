ab6feee5d5d013227040147ea92d2f2e5ec27ea6
jeanraymonddaher <jeanraymond.daher@live.com>
2014-10-17 18:51:59 -0400

    added editing scene buttons

--- src/com/soen6441/ui/scene/EditingScene.java
+++ src/com/soen6441/ui/scene/EditingScene.java
@@ -41,32 +41,33 @@ public class EditingScene extends View{
 		this.controlButton.setLocation(10, 10);
 		this.controlButton.setTitle("Validate");
 		this.controlButton.setSize(120,40);
-		
+		this.add(controlButton);
 		//InfoLabel : valid or not 
 		String valid="Not valid";
 		this.infoLabel=new Label();
 		this.infoLabel.setText("Validation : "+valid);
 		this.infoLabel.setSize(500, 40);
 		this.infoLabel.setLocation(135, 10);
-		
+		this.add(infoLabel);
 		//initial money Label
 	    Label initialMoney=new Label();
 	    initialMoney.setText("Initial Money");
 	    initialMoney.setSize(300, 40);
 	    initialMoney.setLocation(600, 10);
-	    
+	    this.add(initialMoney);
 	    //actual money
 	    this.money=new TextField();
 	    this.money.setText("1000");
 	    //THIS SHOULD BE SET TO PLAY.GETCOINS()
 	    this.money.setSize(80,40);
 	    this.money.setLocation(700,10);
-	    
+	    this.add(money);
 		//back button
 	    this.saveButton=new Button();
 	    this.saveButton.setTitle("SAVE");
 	    this.saveButton.setSize(60, 20);
 	    this.saveButton.setLocation(730, 550);
+	    this.add(saveButton);
 	    //save button
 	    this.backButton=new Button();
 	    this.backButton.setTitle("BACK");
@@ -77,10 +78,12 @@ public class EditingScene extends View{
 	    
 	    //this.mapView=new MapView();
 		this.inspectorView=new InspectorView();	
+		this.add(inspectorView);
 	
 		View view = new View();
 		view.setLocation(70, 10);
 		view.setSize(550, 550);
+		this.add(view);
 		
 		
 		
