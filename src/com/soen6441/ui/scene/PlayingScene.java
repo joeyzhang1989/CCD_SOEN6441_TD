4b549e73363c832d3929cdcd1289b3c0a4790a95
chenglong zhang <chenglongzhang931@gmail.com>
2014-10-16 21:31:54 -0400

    Revert "update PlayingScene class"
    This reverts commit 803127e34d3c27f279219b0ee6439e107d0886bd.

--- src/com/soen6441/ui/scene/PlayingScene.java
+++ src/com/soen6441/ui/scene/PlayingScene.java
@@ -5,20 +5,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
-import com.soen6441.core.Play;
 import com.soen6441.ui.common.Command;
 import com.soen6441.ui.common.IInspectable;
 import com.soen6441.ui.map.MapView;
 import com.soen6441.ui.parallel.Button;
 import com.soen6441.ui.parallel.Label;
 import com.soen6441.ui.parallel.TextField;
+
 import com.soen6441.ui.parallel.View;
-import com.soen6441.ui.parallel.ViewFlow;
 import com.soen6441.ui.parallel.Window;
-
-import demo.soen6441.ui.viewFlow.ViewFlowDemoScene1;
-import demo.soen6441.ui.viewFlow.ViewFlowDemoScene2;
-
 
 
 
@@ -49,10 +44,7 @@ public class PlayingScene extends View{
 	
 	private MapView mapView;
 	
-	private TextField grid,
-					  inspector,
-					  money,
-					  life;
+	private TextField grid,inspector;
 	
 /**
  * override the method initSubviews in the super class View
@@ -77,36 +69,17 @@ public class PlayingScene extends View{
 		this.infoLabel.setLocation(140, 10);
 		this.add(infoLabel);
 		
-		//moneylabel
 		moneyLabel = new Label();
 		moneyLabel.setText("Money");
 		moneyLabel.setSize(120, 40);
-		moneyLabel.setLocation(400, 10);
-		this.add(moneyLabel);   
+		moneyLabel.setLocation(600, 10);
+		this.add(moneyLabel);    
 		
-		//textfield to store the coins change
-		this.money=new TextField();
-		this.money.setText("1000");
-		Play pl = Play.currentPlay();
-		//THIS SHOULD BE SET TO PLAY.GETCOINS()
-		this.money.setSize(80,40);
-		this.money.setLocation(520,10);
-		this.add(money);
-		
-		//lifelabel
 		lifelabel = new Label();
 		lifelabel.setText("Life");
 		lifelabel.setSize(120, 40);
-		lifelabel.setLocation(640, 10);
+		lifelabel.setLocation(720, 10);
 		this.add(lifelabel);   
-		
-		//textfield to store the life change
-		this.life=new TextField();
-		this.life.setText("1000");
-		//THIS SHOULD BE SET TO PLAY.GETLIFE()
-		this.life.setSize(80,40);
-		this.life.setLocation(720,10);
-		this.add(life);
 		
 		//Save button
 		saveButton=new Button();
@@ -156,11 +129,8 @@ public class PlayingScene extends View{
 			//this.add(view);
 	}
 	public static void main(String[] args) {
-		//PlayingScene ps = new PlayingScene();
-		//new Window(ps);
-		ViewFlow viewFlow = new ViewFlow();
-		viewFlow.push(new PlayingScene());
-		new Window(viewFlow);
+		PlayingScene so = new PlayingScene();
+		new Window(so);
 	}
 	
 	
@@ -168,10 +138,7 @@ public class PlayingScene extends View{
 	@Override
 	protected void initEvents() {
 		super.initEvents();
-		/**
-		 * perform the nextWave function to create the next wave of critters
-		 * 
-		 */
+		
 		controlButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -179,159 +146,51 @@ public class PlayingScene extends View{
 			}
 		});
 		
-		/**
-		 * perform the exit function to go back to the main scene
-		 * 
-		 */
-		backButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				 MainScene ms= new  MainScene ();
-				//wait the MainScene to finish implementation
-				 PlayingScene.this.viewFlow.push(ms);
-				
-			}
-		});
-		
-		/**
-		 * perform the save function to save the game for next load
-		 * 
-		 */
-		saveButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("action");
-			}
-		});
-		
-		
 	}
 	
 	//
 	
 	/**
-	 * @author chenglong zhang 
+	 * 
 	 * inner class that implements the IInspectable interface to capture 
 	 * the event of mouse to change the value of the label, buttons etc.
-	 * selecting tower scene the inspector view should update 
 	 */
 	private class SelectTower implements IInspectable
 	{
 
 		@Override
 		public String title() {
-			
+			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public String subtitle() {
-			
+			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public String description() {
-			
+			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public List<Command> commands() {
-			
+			// TODO Auto-generated method stub
 			return null;
 		}
 
 		@Override
 		public void execute(Command command) {
+			// TODO Auto-generated method stub
 			
-			
-		}		
-	}	
-
-
-
-/**
- * @author chenglong zhang 
- * inner class that implements the IInspectable interface to capture 
- * the event of mouse to change the value of the label, buttons etc.
- * selecting a road scene that the inspector view should update 
- */
-private class SelectRoad implements IInspectable
-{
-
-	@Override
-	public String title() {
-		
-		return null;
-	}
-
-	@Override
-	public String subtitle() {
-		
-		return null;
-	}
-
-	@Override
-	public String description() {
-		
-		return null;
-	}
-
-	@Override
-	public List<Command> commands() {
-		
-		return null;
-	}
-
-	@Override
-	public void execute(Command command) {
+		}
 		
 		
 	}
 	
+	
 }
-
-
-/**
- * @author chenglong zhang 
- * inner class that implements the IInspectable interface to capture 
- * the event of mouse to change the value of the label, buttons etc.
- * selecting a empty space scene that the inspector view should update 
- */
-private class SelectEmptySpace implements IInspectable
-{
-
-	@Override
-	public String title() {
-		
-		return null;
-	}
-
-	@Override
-	public String subtitle() {
-		
-		return null;
-	}
-
-	@Override
-	public String description() {
-		
-		return null;
-	}
-
-	@Override
-	public List<Command> commands() {
-		
-		return null;
-	}
-
-	@Override
-	public void execute(Command command) {
-		
-		
-	}
-  }
-}
-
 
