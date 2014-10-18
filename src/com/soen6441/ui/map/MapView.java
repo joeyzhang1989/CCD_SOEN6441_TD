9bd1a398084c70a63a793eb4e7eef773cafb34d4
jeanraymonddaher <jeanraymond.daher@live.com>
2014-10-17 20:09:34 -0400

    implementation of grid view methods

--- src/com/soen6441/ui/map/MapView.java
+++ src/com/soen6441/ui/map/MapView.java
@@ -6,41 +6,38 @@ import com.soen6441.ui.common.GridView;
 //not finish
 /**
  * @author jean raymond daher
- * @author Chenglong Zhang 
+ * @author Chenglong Zhang
  */
-public class MapView extends GridView{
+public class MapView extends GridView {
 
-	//here is the main map of the game
-	//this class should draw the map view of the game 
-	//a place for the grid , for the inspector also
-	
-	
+	// here is the main map of the game
+	// this class should draw the map view of the game
+	// a place for the grid , for the inspector also
+
 	private GridMap map;
-	
+
 	private MapViewListener eventListener;
-	
-	
+
 	public GridMap getMap() {
 		return map;
 	}
-	
+
 	public void setMap(GridMap map) {
 		this.map = map;
 	}
-	
+
 	public MapViewListener getEventListener() {
 		return eventListener;
 	}
-	
+
 	public void setEventListener(MapViewListener eventListener) {
 		this.eventListener = eventListener;
 	}
-	
-	public int suggestedSize()
-	{
-		
+
+	public int suggestedSize() {
+
 		return 0;
-		
+
 	}
-	
+
 }
--- src/com/soen6441/ui/scene/EditingScene.java
+++ src/com/soen6441/ui/scene/EditingScene.java
@@ -1,6 +1,7 @@
 package com.soen6441.ui.scene;
 
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -35,55 +36,86 @@ public class EditingScene extends View{
 	protected void initSubviews() {
 		super.initSubviews();
 		
+		View upperView = new View();
+		upperView.setLocation(0, 0);
+		upperView.setSize(800, 60);
+		upperView.setBackground(Color.gray);
+		
+		
 		//Validate button
-		//ISSUE SHOWING NULL
 		this.controlButton=new Button();
 		this.controlButton.setLocation(10, 10);
 		this.controlButton.setTitle("Validate");
 		this.controlButton.setSize(120,40);
-		this.add(controlButton);
 		//InfoLabel : valid or not 
 		String valid="Not valid";
 		this.infoLabel=new Label();
 		this.infoLabel.setText("Validation : "+valid);
 		this.infoLabel.setSize(500, 40);
 		this.infoLabel.setLocation(135, 10);
-		this.add(infoLabel);
 		//initial money Label
 	    Label initialMoney=new Label();
 	    initialMoney.setText("Initial Money");
 	    initialMoney.setSize(300, 40);
 	    initialMoney.setLocation(600, 10);
-	    this.add(initialMoney);
 	    //actual money
 	    this.money=new TextField();
 	    this.money.setText("1000");
 	    //THIS SHOULD BE SET TO PLAY.GETCOINS()
 	    this.money.setSize(80,40);
 	    this.money.setLocation(700,10);
-	    this.add(money);
+
+	    upperView.add(this.controlButton);
+	    upperView.add(this.infoLabel);
+	    upperView.add(initialMoney);
+	    upperView.add(this.money);
+	    
+		this.add(upperView);
+
+	 
+	    
+	    View  lowerView= new View();
+	    lowerView.setLocation(0,550);
+	    lowerView.setSize(800, 40);;
+	   // lowerView.setBackground(Color.blue);
+
+	    
+	  
 		//back button
 	    this.saveButton=new Button();
 	    this.saveButton.setTitle("SAVE");
 	    this.saveButton.setSize(60, 20);
-	    this.saveButton.setLocation(730, 550);
-	    this.add(saveButton);
+	    this.saveButton.setLocation(730, 0);
 	    //save button
 	    this.backButton=new Button();
 	    this.backButton.setTitle("BACK");
 	    this.backButton.setSize(60, 20);
-		this.backButton.setLocation(10, 550);
-		this.add(this.backButton);
+		this.backButton.setLocation(10, 0);
+		
+		lowerView.add(this.saveButton);
+		lowerView.add(this.backButton);
+		lowerView.setBackground(Color.cyan);
+	    this.add(lowerView);
 	    
-	    
+		
 	    //this.mapView=new MapView();
 		this.inspectorView=new InspectorView();	
+		inspectorView.setLocation(620, 60);
+		inspectorView.setSize(180,480);
+		inspectorView.setBackground(new Color(0xEEEEEE));
 		this.add(inspectorView);
+		
 	
+<<<<<<< Updated upstream
 		View view = new View();
 		view.setLocation(70, 10);
 		view.setSize(550, 550);
 //		this.add(view);
+=======
+	//	View view = new View();
+		//view.setLocation(70, 10);
+		//view.setSize(550, 550);
+>>>>>>> Stashed changes
 		
 		
 		
