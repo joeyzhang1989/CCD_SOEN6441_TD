ed3b7e2be9eecd27c9da3bf9411f2baadec4bf2f
zhaozhe <zhaozheid@126.com>
2014-10-24 16:15:07 -0400

    Selection Bug in GridView fixed
    Selection Bug in GridView fixed

--- src/com/soen6441/ui/common/GridView.java
+++ src/com/soen6441/ui/common/GridView.java
@@ -162,6 +162,10 @@ public class GridView extends View {
 		this.remove(cells[row][column]);
 		
 		this.add(newCell);
+		
+		if (cell == this.selectedCell) {
+			setSelectedCell(newCell);
+		}
 	}
 
 	/*
