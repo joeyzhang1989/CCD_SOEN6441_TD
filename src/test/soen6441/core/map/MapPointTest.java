7368d5a3604e88a4f668dad6cc7764fad2eae855
zhaozhe <zhaozheid@126.com>
2014-10-25 19:15:44 -0400

    SceneryCell displaying on windows bug fixed
    SceneryCell displaying on windows bug fixed

--- src/test/soen6441/core/map/MapPointTest.java
+++ src/test/soen6441/core/map/MapPointTest.java
@@ -2,23 +2,36 @@ package test.soen6441.core.map;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import com.soen6441.core.map.MapPoint;
+
 public class MapPointTest {
+	
+	MapPoint p1;
+	MapPoint p2;
+	
+	@Before
+	public void setup() {
+		p1 = new MapPoint(10.2, 10.4);
+		p2 = new MapPoint(10.2, 10.4);
+		
+	}
 
 	@Test
 	public void testGetGridedX() {
-		fail("Not yet implemented");
+		assertSame(p1.getGridedX(), 10);
 	}
 
 	@Test
 	public void testGetGridedY() {
-		fail("Not yet implemented");
+		assertSame(p1.getGridedY(), 10);
 	}
 
 	@Test
 	public void testEqualsObject() {
-		fail("Not yet implemented");
+		assertEquals(p1, p2);
 	}
 
 }
--- src/test/soen6441/core/map/MapPointTestFunctional.java
+++ src/test/soen6441/core/map/MapPointTestFunctional.java
@@ -8,7 +8,7 @@ public class MapPointTestFunctional {
 
 	@Test
 	public void testDistanceTo() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
