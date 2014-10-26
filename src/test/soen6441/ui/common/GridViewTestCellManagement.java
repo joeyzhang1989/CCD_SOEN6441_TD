8e73b48f54b9279ce7b9641f0c66666fa2b1b82d
jeanraymonddaher <jeanraymond.daher@live.com>
2014-10-26 15:56:05 -0400

    incomplete testing *
    taking a break will resume in a bit

--- src/test/soen6441/ui/common/GridViewTestCellManagement.java
+++ src/test/soen6441/ui/common/GridViewTestCellManagement.java
@@ -12,7 +12,7 @@ import com.soen6441.ui.common.GridViewCell;
 public class GridViewTestCellManagement {
 
 	private GridView one;
-	private GridViewCell cellA, cellB, cellC;
+	private GridViewCell cellA, cellB, cellC, cellD;
 	private GridPoint gp1, gp2;
 
 	@Before
@@ -21,42 +21,33 @@ public class GridViewTestCellManagement {
 		cellA = new GridViewCell();
 		cellB = new GridViewCell();
 		cellC = new GridViewCell();
+		cellD= new GridViewCell();
 		gp1 = new GridPoint(0, 0);
 		gp2 = new GridPoint(4, 4);
 	}
 
-	private void oldTest(){
-		this.one.addCell(cellA, gp1);
-		assertEquals(one.getCell(0, 0), cellA);
-		this.one.removeCell(cellA);
-		assertEquals(null, one.getCell(0, 0));
-		
-		this.one.addCell(cellB, gp2);
-		this.one.replaceCell(cellB, cellC);
-		assertEquals(one.getCell(4, 4),cellC);
-		
-		assertEquals(one.getNumberOfColumns(),5);
-		assertEquals(one.getNumberOfRows(),5);
-	}
-	
 	@Test
 	public void testAddCell() {
-		fail("Not yet implemented");
+		this.one.addCell(cellA, gp1);
+		assertEquals(one.getCell(0, 0), cellA);
 	}
 
 	@Test
 	public void testGetCell() {
-		fail("Not yet implemented");
+		assertEquals(one.getCell(0, 0),cellA);
 	}
 
 	@Test
 	public void testRemoveCell() {
-		fail("Not yet implemented");
+		this.one.removeCell(cellA);
+		assertEquals(null, one.getCell(0, 0));
 	}
-
+	
 	@Test
 	public void testReplaceCell() {
-		fail("Not yet implemented");
+		this.one.addCell(cellD, gp2);
+		this.one.replaceCell(cellD, cellC);
+		assertEquals(one.getCell(4, 4),cellC);
 	}
 
 }
