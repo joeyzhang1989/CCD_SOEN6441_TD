a07fdaa1792c2b0d8834693c9c50b15e80296c7f
Mohammad Ali <aliraja4@gmail.com>
2014-10-26 18:58:24 -0400

    MultipleStartPoints() test for EndPointQuantutyValidatorTest

--- src/test/soen6441/core/map/validator/EndPointQuantityValidatorTest.java
+++ src/test/soen6441/core/map/validator/EndPointQuantityValidatorTest.java
@@ -1,6 +1,7 @@
 package test.soen6441.core.map.validator;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -11,7 +12,6 @@ import com.soen6441.core.map.MapPoint;
 import com.soen6441.core.map.PathValidator;
 import com.soen6441.core.map.Road;
 import com.soen6441.core.map.validator.EndPointQuantityValidator;
-import com.soen6441.core.map.validator.StartPointQuantityValidator;
 
 public class EndPointQuantityValidatorTest {
 
@@ -51,4 +51,23 @@ public class EndPointQuantityValidatorTest {
 		r2 = null;
 	}
 	
+	/*
+     * testMultipleStartPoints() will test EndPointQuantityValidator when there are more than one startPoint on the map.
+     * 
+     */
+	
+	@Test
+	public void testMultipleStartPoints() {
+		p1 = new MapPoint(1, 1);
+		p2 = new MapPoint(2, 2);
+		r1 = new Road(Road.Type.END);
+		r2 = new Road(Road.Type.END);
+		map.setItem(r1, p1);
+		map.setItem(r2, p2);
+		validator.setMap(map);
+		
+		assertFalse(validator.validate());	// EndPointQuantityValidator must return False.
+		assertEquals(validator.getErrorMessage(),"There can't be more than one end point in the map");
+		validator.setErrorMassage(null);	//Set the error Message to null for followong tests
+	}
 }
