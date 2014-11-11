77f7ba86b59363d92404fb773e18573e79a740fa
zhaozhe <zhaozheid@126.com>
2014-11-11 14:03:29 -0500

    Critter
    Critter

--- src/com/soen6441/core/critter/CritterManager.java
+++ src/com/soen6441/core/critter/CritterManager.java
@@ -1,26 +1,20 @@
 package com.soen6441.core.critter;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-import com.soen6441.core.tower.Tower;
-import com.soen6441.core.tower.TowerManager.NameForArchiving;
-
 public class CritterManager {
 
-	private String towerType;
+	private String critterName;
+
 	private String filePath;
 
 	private Critter critter;
 	
-	public CritterManager(String towerType, String filePath) {
-		this.towerType = towerType;
+	public CritterManager(String critterName, String filePath) {
+		this.critterName = critterName;
 		this.filePath = filePath;
 		this.analyse();
 	}
@@ -39,66 +33,19 @@ public class CritterManager {
 		
 		Element root = document.getRootElement();
 		
-		this.decode(root);
 		
 	}
 	
-	public class NameForArchiving{
-		
-		public static final String Class = "CritterManager";
-		private static final String Speed="speed";
-		private static final String Reward="reward";
-		private static final String Hp="hp";
-		
-		
-	}
-	
-	private Critter generateCritter(String typeName) {
-		Critter critter = null;
-		try {
-			critter = (Critter) (Class.forName("com.soen6441.core.critter." + typeName).newInstance());
-		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
-			e.printStackTrace();
-		} 
-		return critter;
-	}
-	
-	public void decode(Element root) {
-		
-		Element critterManagerElement = root.element(NameForArchiving.Class);
-       // this.initialPrice = (Integer.parseInt(towerManagerElement.element(NameForArchiving.InitialPrice).getText()));
-        Element critterElement = critterManagerElement.element(NameForArchiving.Critter);
-        
-        //Generate tower depending on towerType property of this class.
-        for ( @SuppressWarnings("rawtypes")Iterator i = critterElement.elementIterator(); i.hasNext(); ) {
-        	
-            Element element = (Element)i.next();
-                 
-            Critter c= this.generateCritter(towerType);
-            
-            //c.setManager(this);
-    		
-    		c.decode(element);
-    		
-    		this.critter=c;
-    		
-    		//Critter.add(c);
-           	
-        }
-		
-	}
-	
-	public Critter generateCritter(CritterMultiplier multiplier) {
+	public Critter generateCritter(CritterMultiplier critterMultiplier) {
 		return null;
 	}
 
-	public String getTowerType() {
-		return towerType;
+	public String getCritterName() {
+		return critterName;
 	}
 
-	public void setTowerType(String towerType) {
-		this.towerType = towerType;
+	public void setCritterName(String critterName) {
+		this.critterName = critterName;
 	}
 
 	public String getFilePath() {
