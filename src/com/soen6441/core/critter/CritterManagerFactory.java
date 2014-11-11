1bdf7024f329b7a56fd6c3a0566c973437fd5aaa
Jean Raymond Daher <jeanraymond.daher@live.com>
2014-11-10 21:45:55 -0500

    smallfix

--- src/com/soen6441/core/critter/CritterManagerFactory.java
+++ src/com/soen6441/core/critter/CritterManagerFactory.java
@@ -1,10 +1,15 @@
 package com.soen6441.core.critter;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.soen6441.core.tower.TowerManager;
 
 public class CritterManagerFactory {
 
 	private List<String> typeNames;
+	private Map<String, CritterManager> managers = new HashMap<String, CritterManager>();
 	
 	public static CritterManagerFactory defaultFactory(){
 		return null;
