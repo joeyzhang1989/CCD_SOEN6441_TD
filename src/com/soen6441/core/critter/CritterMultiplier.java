f0277c217f537e4533bde2427552d1783d58bc94
Jean Raymond Daher <jeanraymond.daher@live.com>
2014-11-10 21:20:42 -0500

    critter multiplier implementation

--- src/com/soen6441/core/critter/CritterMultiplier.java
+++ src/com/soen6441/core/critter/CritterMultiplier.java
@@ -2,4 +2,28 @@ package com.soen6441.core.critter;
 
 public class CritterMultiplier {
 
+	private String critterName;
+	private double hpMultiplier;
+	private double speedMultiplier;
+	
+	public String getCritterName() {
+		return critterName;
+	}
+	public void setCritterName(String critterName) {
+		this.critterName = critterName;
+	}
+	public double getHpMultiplier() {
+		return hpMultiplier;
+	}
+	public void setHpMultiplier(double hpMultiplier) {
+		this.hpMultiplier = hpMultiplier;
+	}
+	public double getSpeedMultiplier() {
+		return speedMultiplier;
+	}
+	public void setSpeedMultiplier(double speedMultiplier) {
+		this.speedMultiplier = speedMultiplier;
+	}
+	
+	
 }
