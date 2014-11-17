c03d0220e9ebc92d08c3f71dcafb5be1110c7c9d
zhaozhe <zhaozheid@126.com>
2014-11-17 13:26:01 -0500

    Test Suites Updates
    Test Suites Updates

---
+++ src/test/soen6441/core/critter/SuiteCritter.java
@@ -0,0 +1,11 @@
+package test.soen6441.core.critter;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ CritterManagerTest.class })
+public class SuiteCritter {
+
+}
