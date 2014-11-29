93fb8e488864e4b8c5d7ab8e9a1f73fdf22754f3
Jean Raymond Daher <jeanraymond.daher@live.com>
2014-11-29 14:55:07 -0500

    add unimplemented idea - along with save

--- src/com/soen6441/ui/scene/EditingScene.java
+++ src/com/soen6441/ui/scene/EditingScene.java
@@ -3,6 +3,8 @@ package com.soen6441.ui.scene;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -156,6 +158,38 @@ public class EditingScene extends View implements GridViewSelectionListener {
 	@Override
 	protected void initEvents() {
 		this.mapView.setSelectionListener(this);
+		
+		/*
+		this.mapView.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {
+				System.out.println("key preessed");
+				int keyCode= e.getKeyCode();
+				if(keyCode==114)
+				{
+					Road x= new Road();
+					GridMap gridMap = play.getMap();
+					gridMap.setItem(x, gridMap.getSelectedPoint());
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		
+		});
+*/
+			
+	
 		this.saveButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
